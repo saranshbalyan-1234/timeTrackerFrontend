687a829f1330fda6fc0a4270b14061f1e0bbfbfb
saransh balyan <saransh@kditsolutions.com>
2022-08-05 01:32:25 +0530

    reset password screen

---
+++ src/Auth/PasswordReset.js
@@ -0,0 +1,83 @@
+import React, { useState } from "react";
+import { Form, Input, Button, Card, Spin } from "antd";
+import { UserOutlined } from "@ant-design/icons";
+import { useNavigate } from "react-router-dom";
+import { StyledWrapper } from "./style";
+import { connect } from "react-redux";
+import { resetPassword } from "../Redux/Actions/auth";
+const PasswordReset = ({ resetPassword }) => {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleReset = async () => {
+    setLoading(true);
+
+    await resetPassword({ email: email });
+    setLoading(false);
+  };
+  return (
+    <StyledWrapper>
+      <div className="outsideApp">
+        <Spin spinning={loading}>
+          <Card title="Reset Your Password" bordered class="shadow">
+            <img
+              alt="logo"
+              src="/Logo/logo2.svg"
+              style={{ height: "50px", marginBottom: "10px" }}
+            />
+            <Form
+              name="normal_login"
+              className="login-form"
+              initialValues={{ remember: true }}
+              onFinish={handleReset}
+            >
+              <Form.Item
+                name="email"
+                rules={[
+                  { required: true, message: "Please input your Email!" },
+                ]}
+              >
+                <Input
+                  type="email"
+                  prefix={<UserOutlined className="site-form-item-icon" />}
+                  placeholder="Email"
+                  name="email"
+                  onChange={(e) => {
+                    setEmail(e.target.value);
+                  }}
+                />
+              </Form.Item>
+
+              <Form.Item>
+                <Button
+                  type="primary"
+                  htmlType="submit"
+                  className="login-form-button"
+                  style={{ marginRight: "10px" }}
+                >
+                  Reset
+                </Button>{" "}
+                Or
+                <Button
+                  onClick={() => {
+                    navigate("/signin");
+                  }}
+                  htmlType="button"
+                  className="login-form-button"
+                  style={{ marginLeft: "10px" }}
+                >
+                  Login
+                </Button>
+              </Form.Item>
+            </Form>
+          </Card>
+        </Spin>
+      </div>
+    </StyledWrapper>
+  );
+};
+
+const mapDispatchToProps = { resetPassword };
+
+export default connect(null, mapDispatchToProps)(PasswordReset);
